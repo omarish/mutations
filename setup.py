b3e38d1c3009a5d10e64408e6932b4c9bee3f3ef
Omar Bohsali <omar.bohsali@gmail.com>
2018-02-09 09:33:12 -0800

    Adds setup.cfg.

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from distutils.core import setup
 import mutations
 
 setup(
-    name='Mutations',
+    name='mutations',
     version=mutations.__version__,
     author='Omar Bohsali',
     author_email='me@omarish.com',
     packages=['mutations'],
     url="http://github.com/omarish/mutations",
-    license='LICENSE',
-    description='Encapsulate your business logic.',
+    license='MIT',
+    description='Encapsulate your business logic in command classes.',
     long_description="Mutations helps you split your complex business logic into command classes which are easier to test and maintain.",
+    keywords=['business logic', 'django', 'fat models', 'commands', 'validation'],
     install_requires=[],
 )
