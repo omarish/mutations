f843b4e209775dfc84e43f06bde1a69adbef71c8
Omar Bohsali <omar.bohsali@gmail.com>
2018-02-09 09:48:59 -0800

    v0.2.1.

--- mutations/__init__.py
+++ mutations/__init__.py
@@ -1,3 +1,3 @@
 from .core import Mutation
 
-__version__ = '0.2'
+__version__ = '0.2.1'
--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     license='MIT',
     description='Encapsulate your business logic in command classes.',
     long_description="Mutations helps you split your complex business logic into command classes which are easier to test and maintain.",
-    keywords=['business logic', 'django', 'fat models', 'commands', 'validation'],
+    keywords=['business logic', 'django', 'fat models', 'thin models', 'input validation', 'commands', 'validation'],
     install_requires=[],
 )
